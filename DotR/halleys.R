4a496fa75cf8f16d3f8f6400473e3fbe67ecb9f9
Martin J Frigaard <mjfrigaard@users.noreply.github.com>
2018-06-06 20:29:00 -0700

    working with date/times data

---
+++ DotR/halleys.R
@@ -0,0 +1,70 @@
+halleys <- structure(list(designation = c(
+  "1P/66 B1, 66", "1P/141 F1, 141",
+  "1P/218 H1, 218", "1P/295 J1, 295", "1P/374 E1, 374", "1P/451 L1, 451",
+  "1P/530 Q1, 530", "1P/607 H1, 607", "1P/684 R1, 684", "1P/760 K1, 760",
+  "1P/837 F1, 837", "1P/912 J1, 912", "1P/989 N1, 989", "1P/1066 G1, 1066",
+  "1P/1145 G1, 1145", "1P/1222 R1, 1222", "1P/1301 R1, 1301", "1P/1378 S1, 1378",
+  "1P/1456 K1, 1456", "1P/1531 P1, 1531", "1P/1607 S1, 1607", "1P/1682 Q1, 1682",
+  "1P/1758 Y1, 1759 I", "1P/1835 P1, 1835 III", "1P/1909 R1, 1910 II, 1909c",
+  "1P/1982 U1, 1986 III, 1982i", NA
+), year = c(
+  66L, 141L, 218L,
+  295L, 374L, 451L, 530L, 607L, 684L, 760L, 837L, 912L, 989L, 1066L,
+  1145L, 1222L, 1301L, 1378L, 1456L, 1531L, 1607L, 1682L, 1758L,
+  1835L, 1910L, 1986L, 2061L
+), perihelion_date = structure(c(
+  -695396,
+  -667945, -639810, -611685, -582884, -554620, -525631, -497742,
+  -469372, -441783, -413764, -386219, -358058, -330096, -301217,
+  -272949, -244053, -215911, -187728, -160105, -132284, -104932,
+  -77360, -48989, -21806, 5883, 33446
+), class = "Date"), start_date = structure(c(
+  -695397,
+  -667948, -639810, -611685, -582884, -554625, -525680, -497753,
+  -469427, -441804, -413764, -386228, -358058, -330179, -301221,
+  -272949, -244053, -215911, -187728, -160105, -132284, -104932,
+  -77360, -49096, -21806, 5883, 33446
+), class = "Date"), end_date = structure(c(
+  -695396,
+  -667945, -639769, -611672, -582881, -554620, -525631, -497742,
+  -469372, -441783, -413761, -386219, -358055, -330096, -301217,
+  -272931, -244044, -215906, -187575, -160105, -132284, -104932,
+  -77073, -48989, -21776, 5883, 33446
+), class = "Date"), distance = c(
+  NA,
+  NA, NA, NA, "0.09 AU", NA, NA, "0.09 AU", NA, NA, "0.03 AU",
+  NA, NA, "0.10 AU", NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
+  "0.586 AU", NA
+)), .Names = c(
+  "designation", "year", "perihelion_date",
+  "start_date", "end_date", "distance"
+), class = c(
+  "tbl_df", "tbl",
+  "data.frame"
+), row.names = c(NA, -27L), spec = structure(list(
+  cols = structure(list(designation = structure(list(), class = c(
+    "collector_character",
+    "collector"
+  )), year = structure(list(), class = c(
+    "collector_integer",
+    "collector"
+  )), perihelion_date = structure(list(format = ""), .Names = "format", class = c(
+    "collector_date",
+    "collector"
+  )), start_date = structure(list(format = ""), .Names = "format", class = c(
+    "collector_date",
+    "collector"
+  )), end_date = structure(list(format = ""), .Names = "format", class = c(
+    "collector_date",
+    "collector"
+  )), distance = structure(list(), class = c(
+    "collector_character",
+    "collector"
+  ))), .Names = c(
+    "designation", "year", "perihelion_date",
+    "start_date", "end_date", "distance"
+  )), default = structure(list(), class = c(
+    "collector_guess",
+    "collector"
+  ))
+), .Names = c("cols", "default"), class = "col_spec"))
